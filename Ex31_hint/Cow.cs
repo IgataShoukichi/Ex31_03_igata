09128d6c82d67721db5ea12c7a0e660fba1403bf
igatashoukichi@gmail.com <igatashoukichi@gmail.com>
2022-11-17 14:32:24 +0900

    変更

---
+++ Ex31_hint/Cow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex31_hint
+{
+    class Cow : Character
+    {
+        public Cow(string name, float hp, float power) : base(name, hp, power) { }
+
+    }
+}
